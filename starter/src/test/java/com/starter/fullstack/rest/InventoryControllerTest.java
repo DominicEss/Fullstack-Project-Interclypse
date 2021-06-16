b1c12ea23d9c5af36c5a54c10e6ae39be1ae8958
dominic.ess <de5@umbc.edu>
2021-06-16 19:48:19 -0400

    Working with debug statements & checkstyle errors

--- starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
+++ starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
@@ -14,8 +14,10 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -56,7 +58,7 @@ public class InventoryControllerTest {
   public void create() throws Throwable {
     // Create inventory objet to test with
     this.inventory = new Inventory();
-    this.inventory.setId("test");
+    this.inventory.setId(null);
     this.inventory.setName("ALSO TEST");
     this.inventory.setProductType("Beer");
 
@@ -69,6 +71,32 @@ public class InventoryControllerTest {
 
     // test to make sure that the inventory object was posted
     Assert.assertEquals(2, this.mongoTemplate.findAll(Inventory.class).size());
+
+    List<Inventory> items = mongoTemplate.findAll(Inventory.class);
+    System.out.println("\n\n\n\n" + items + "\n\n\n\n");
+
+
+  }
+
+ /**
+   * Test delete endpoint.
+   * @throws Throwable see MockMvc
+   */
+  @Test
+  public void remove() throws Throwable { 
+    Assert.assertEquals(1, this.mongoTemplate.findAll(Inventory.class).size());
+
+    List<Inventory> items = mongoTemplate.findAll(Inventory.class);
+    System.out.println("\n\n\n\n" + items + "\n\n\n\n");
+
+    this.mockMvc.perform(delete("/inventory")
+//        .param("id",this.inventory.getId())
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(this.inventory.getId()))
+      .andExpect(status().isOk());
+
+    Assert.assertEquals(0, this.mongoTemplate.findAll(Inventory.class).size());
   }
 }
 
